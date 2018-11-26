b9e4e72bcfe9f2fd8279d2f5c003bf4fad4c05e3
sekiya9311 <sekiya9311@gmail.com>
2018-11-26 23:26:40 +0900

    add AlarmAppPage

---
+++ lib/alarm_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlarmApp extends StatefulWidget {
+  
+  @override
+  State<StatefulWidget> createState()  => _AlarmAppState();
+}
+
+class _AlarmAppState extends State<AlarmApp> {
+  @override
+    Widget build(BuildContext context) {
+      // TODO: implement build
+      return Scaffold();
+    }
+}
